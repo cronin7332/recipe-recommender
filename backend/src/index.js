c38730d190e74278b38f97996eaee1aba42a48ea
cronin7332 <chandrakantrohit@gmail.com>
2024-03-11 00:37:06 +0530

    Frontend/Backend Ready

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://cronin7332:croninMERN@recipes.4apqi2d.mongodb.net/?retryWrites=true&w=majority&appName=recipes"
+);
+
+app.listen(3001, () => console.log("Server started"));
